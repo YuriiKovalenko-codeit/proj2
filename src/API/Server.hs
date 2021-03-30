4a954c02237bd440dee2d199872ac6a84be903db
Yuri Kovalenko <yurii.kovalenko@codeit.pro>
2021-03-30 21:40:02 +0300

    Added Swagger UI support on /swagger-ui endpoint

--- src/API/Server.hs
+++ src/API/Server.hs
@@ -3,12 +3,27 @@ module API.Server where
 import API
 import AuthenticatedUser
 import Data.Proxy
+import Data.Swagger
+import Servant.API
 import Servant.Auth as SA
+import Servant.Auth.Swagger
+import Servant.Swagger
+import Servant.Swagger.UI
 
-type API =
+type API = SwaggerAPI :<|> BasicAPI
+
+api :: Proxy API
+api = Proxy
+
+type BasicAPI =
   API'
     (Auth '[SA.BasicAuth] AuthenticatedUser)
     (Auth '[SA.BasicAuth, SA.JWT] AuthenticatedUser)
 
-api :: Proxy API
-api = Proxy
+basicAPI :: Proxy BasicAPI
+basicAPI = Proxy
+
+type SwaggerAPI = SwaggerSchemaUI "swagger.json" "swagger-ui"
+
+swaggerDoc :: Swagger
+swaggerDoc = toSwagger basicAPI
